f48900dbff8ed1b7f0460d9d2d7daef24b3c8bf9
郑清文 <1216483452@qq.com>
2019-04-19 10:34:00 +0800

    first commit

---
+++ day01/basa_type.py
@@ -0,0 +1,65 @@
+# def nimasile():
+#     print('fff')
+# def int_demo():
+#     aint = 5
+#     type(aint)
+#     print(type(aint))
+#     print(aint)
+# def str_demo():
+#     astr = '5'
+#     type(astr)
+#     print(astr)
+#     print(type('astr'))
+# def float_demo():
+#     afloat = 0.5
+#     type(afloat)
+#     print(afloat)
+#     print(type(afloat))
+def add_demo(a,b):
+    print(a+b)
+# def add_demo(a,b):
+#     print(a+b)
+# def type_zhuanhuan():
+#     zhuanhuan=996
+#
+#     print(zhuanhuan) ,print(type (str(zhuanhuan))), print(type(int(zhuanhuan))), print(type(zhuanhuan))
+# def zz_pinjie():
+#     a=1223
+#     b='搜索'
+#     c=9527
+#     print(str(a)+b+str(c))
+#     print('%s, %s, %s' % (a,b,c))
+#     print('a是%s b是%s c是%s'% (a,b,c))
+def jian_fa(a,b):
+    c= a-b
+    return c
+
+
+
+
+
+
+
+
+
+
+#
+# if __name__ == '__main__':
+#     c = jian_fa(8, 4)
+#      d_demo(8,6)
+#
+#     print(c)
+#     print(d)
+#     print(type(d))
+
+
+
+
+    # zz_pinjie()
+    # type_zhuanhuan()
+    # add_demo(100,1111)
+    # float_demo()
+    # str_demo()
+    # int_demo()
+    # nimasile()
+    # add_demo(1000000000000000000000000000000000000000,100000000000000000000000000000000000000000)
---
+++ day01/modledy.py
@@ -0,0 +1,13 @@
+# 先导入其他模块 语法：from 包名 import 模块名
+from day01 import basa_type
+
+
+
+
+
+
+if __name__ == '__main__':
+    basa_type.add_demo(5,10)
+    c = basa_type.jian_fa(10,5)
+    print(c)
+
