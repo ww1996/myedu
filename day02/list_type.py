672ead16b889fc586db5d7d325b18a592a5b9b74
郑清文 <1216483452@qq.com>
2019-04-19 12:00:06 +0800

    新增list查询

---
+++ day02/list_type.py
@@ -0,0 +1,23 @@
+
+def list_type(a):
+    print(a[3])
+    print(a[-2])
+    print(a[0:5])
+    print(a[1:5])
+    print(a[0:6])
+    print(a[1:3])
+    print(a[1:4])
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    alist=['你好',2,'shabi','dashabi',250]
+    list_type(alist)
+
